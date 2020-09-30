778af0353611ec456a77f9d51051af6df2346800
izhaoo <894519210@qq.com>
2020-09-30 23:50:37 +0800

    add: empty

---
+++ src/components/empty/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from '@tarojs/components'
+import Iconfont from '@/components/iconfont'
+import './index.scss'
+
+export default function Empty() {
+  return (<View className='empty'>
+    <Iconfont name='iconfile-text' />
+    <Text className='tips'>--- 暂无文章 ---</Text>
+  </View>)
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { View } from '@tarojs/components'
+import { View, Block } from '@tarojs/components'
 import usePagination from '@/hooks/usePagination'
 import PostItem from '@/components/post-item'
+import Empty from '@/components/empty'
 import './home.scss'
 
 export default function Index() {
   const [posts, hasMore] = usePagination()
 
-  return (<View className='home'>
-    {posts && posts.map((item: any) => {
-      return (<PostItem item={item} key={item.slug} />)
-    })}
-    { !hasMore && (<View className='none'>--- 我也是有底线的 ---</View>)}
-  </View>)
+  return (
+    <Block>
+      {posts ? (
+        <View className='home'>
+          {posts.map((item: any) => {
+            return (<PostItem item={item} key={item.slug} />)
+          })}
+          {!hasMore && (<View className='none'>--- 我也是有底线的 ---</View>)}
+        </View>
+      ) : (
+          <Empty />
+        )}
+    </Block>
+  )
 }
