02bb9716cd761252ebb3133c0ef485a34076c334
izhaoo <894519210@qq.com>
2020-10-18 14:31:30 +0800

    feat: post top

--- src/components/post_item/index.tsx
+++ src/components/post_item/index.tsx
@@ -7,10 +7,10 @@ import './index.scss'
 
 export default function PostItem(props: { item }) {
   const { item } = props
-
   return (<View className='post-item' onClick={() => { Taro.navigateTo({ url: `/pages/post/post?slug=${item.slug}` }) }}>
     <Image src={item.cover} lazyLoad className='cover' />
     <View className='content'>
+      {item.top && (<View className='top'><Iconfont name='iconpushpin' /></View>)}
       <Text className='title'>{item.title}</Text>
       <View className='excerpt'>
         <Text>{item.excerpt}</Text>
